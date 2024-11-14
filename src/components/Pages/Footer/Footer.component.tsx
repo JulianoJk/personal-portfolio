177d4bf5807fea667edae599c68e1104c18fe5be
Juliano <67047882+JulianoJk@users.noreply.github.com>
2024-11-14 16:51:18 +0200

    Updated link for cv and page title

--- src/components/Pages/Footer/Footer.component.tsx
+++ src/components/Pages/Footer/Footer.component.tsx
@@ -43,7 +43,7 @@ const FooterSocial = () => {
         </Grid>
         <Grid item>
           <a
-            href="https://drive.google.com/file/d/1Md3vIvywKuM39aaUzxkaaqtj7372IsKr/view?usp=sharing"
+            href="https://drive.google.com/file/d/18luSAuhEPQha01njlHc0fRP5Jpn8Yd2e/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="CV"
